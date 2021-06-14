08b3d563f4411ad539989f68f453d8dbfc5c6bc7
Jefferson <jeffer_16_99@hotmail.com>
2021-06-14 07:47:35 -0500

    Primer commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { ChartsModule } from 'ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { ReportesComponent } from '../../reportes/reportes.component';
+import { FacturacionComponent } from '../../facturacion/facturacion.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ChartsModule,
+    NgbModule,
+    ToastrModule.forRoot()
+  ],
+  declarations: [
+    DashboardComponent,
+    NotificationsComponent,
+    ReportesComponent,
+    FacturacionComponent
+  ]
+})
+
+export class AdminLayoutModule {}
